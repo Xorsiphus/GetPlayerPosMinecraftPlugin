3bed4f3dfeeacae28cdb579c201ef5d02ecdbaa5
Evgeniy <Xorsiphus@yandex.ru>
2021-08-05 09:25:07 +0700

    v1.2

--- src/ru/xorsiphus/positiongetter/Getter.java
+++ src/ru/xorsiphus/positiongetter/Getter.java
@@ -1,10 +1,15 @@
 package ru.xorsiphus.positiongetter;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
 
 
 public class Getter extends JavaPlugin {
@@ -14,7 +19,8 @@ public class Getter extends JavaPlugin {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@Nonnull CommandSender sender, @Nonnull Command command,
+                             @Nonnull String label, @Nonnull String[] args) {
 
         Player player = null;
 
@@ -22,37 +28,58 @@ public class Getter extends JavaPlugin {
             player = (Player) sender;
 
         if (args.length == 1) {
+            Location targetPlayerLoc = null;
+            String message = null;
+
             var targetPlayer = getServer().getPlayer(args[0]);
+
             if (targetPlayer == null) {
-                sender.sendMessage("Player not found!");
-                return true;
+                var offlinePlayers = getServer().getOfflinePlayers();
+
+                for (OfflinePlayer offlinePlayer : offlinePlayers) {
+                    if (Objects.equals(offlinePlayer.getName(), args[0])) {
+                        targetPlayerLoc = offlinePlayer.getBedSpawnLocation();
+                        if (targetPlayerLoc == null) {
+                            sender.sendMessage(ChatColor.RED + "Player is currently offline and did not spawn by the bed!");
+                            return true;
+                        } else {
+                            message = ChatColor.GREEN +
+                                    offlinePlayer.getName() +
+                                    "'s bed at: " +
+                                    Objects.requireNonNull(targetPlayerLoc.getWorld()).getName() +
+                                    " - " + targetPlayerLoc.getBlockX() +
+                                    " " + targetPlayerLoc.getBlockY() +
+                                    " " + targetPlayerLoc.getBlockZ();
+                        }
+                    }
+                }
+            } else {
+                targetPlayerLoc = targetPlayer.getLocation();
+                message = ChatColor.GREEN +
+                        targetPlayer.getName() +
+                        " at: " +
+                        Objects.requireNonNull(targetPlayerLoc.getWorld()).getName() +
+                        " - " + targetPlayerLoc.getBlockX() +
+                        " " + targetPlayerLoc.getBlockY() +
+                        " " + targetPlayerLoc.getBlockZ();
             }
 
-            var targetPlayerLoc = targetPlayer.getLocation();
+            if (targetPlayerLoc == null) {
+                sender.sendMessage(ChatColor.RED + "Player not found!");
+                return true;
+            }
 
             if (player != null) {
                 if (player.isOp()) {
-                    player.sendMessage(
-                            targetPlayerLoc.getWorld().getName() +
-                                    ": " + targetPlayerLoc.getBlockX() +
-                                    " " + targetPlayerLoc.getBlockY() +
-                                    " " + targetPlayerLoc.getBlockZ()
-                    );
+                    player.sendMessage(message);
 
                 } else {
-                    player.sendMessage("You are not an op!");
+                    player.sendMessage(ChatColor.RED + "You are not an op!");
                 }
-                return true;
             } else {
-                sender.sendMessage(
-                        ChatColor.GREEN +
-                                targetPlayerLoc.getWorld().getName() +
-                                ": " + targetPlayerLoc.getBlockX() +
-                                " " + targetPlayerLoc.getBlockY() +
-                                " " + targetPlayerLoc.getBlockZ()
-                );
-                return true;
+                sender.sendMessage(message);
             }
+            return true;
         }
         return false;
     }
